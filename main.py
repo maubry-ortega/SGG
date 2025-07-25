1bf91816c37c942eada79fcf023ba93e35814938
maubry-ortega <ortegamaubry@gmail.com>
2025-07-25 17:34:54 -0500

    update deploy in render

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import create_app
 
 # Crear instancia de la app Flask
-flask_app = create_app()
+app = create_app()
 
 # Ruta simple para probar que Flask funciona
 @flask_app.route("/api/ping")
@@ -10,4 +10,4 @@ def ping():
 
 # Main principal
 if __name__ == "__main__":
-    flask_app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
